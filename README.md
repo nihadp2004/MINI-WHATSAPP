e3cb13b3873598d2ebc15dc81864e3e0b2c18c90
nihadp2004 <nihaddevkdp@gmail.com>
2025-07-04 14:55:31 +0530

    start commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mini_whatsapp_
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/mini_whatsapp_/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mini_whatsapp_
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
